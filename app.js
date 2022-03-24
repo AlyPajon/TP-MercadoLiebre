40e671a3889bcad2bdfb7f610a7ce7e6ab216429
AlyPajon <alejandropajon098@gmail.com>
2022-03-24 17:28:48 -0300

    completado

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(express.static("public"))
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "views", "home.html")))
 
 
-
 app.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`)
 })
